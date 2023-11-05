74150947dda6e102d5e5fef3f80dc8926b435315
matiasgon <nnmatias@gmail.com>
2023-11-05 11:36:40 -0300

    creation of VPC and EC2 for each enviroment

--- infrastructure.tf
+++ infrastructure.tf
@@ -19,15 +19,9 @@ variable "environments" {
   default = ["production", "testing"]
 } */
 
-module "vpc" {
-  providers = {
-    aws = aws.AU
-  }
-  source = "./modules/VPC"
-  vpc_name = "vpc-test"
-  cidr_block = "10.0.0.0/16"
-  public_subnet_cidr_block = "10.0.1.0/24"
-  private_subnet_cidr_block = "10.0.2.0/24"
+variable "environments" {
+  type    = list(string)
+  default = ["production", "testing"]
 }
 
 module "permission" {
@@ -37,15 +31,80 @@ module "permission" {
   source = "./modules/Permission"
 }
 
-module "ec2"{
+module "vpc-au" {
+  providers = {
+    aws = aws.AU
+  }
+  for_each = toset(var.environments)
+  source = "./modules/VPC"
+  vpc_name = "vpc-${each.value}"
+  cidr_block = "10.1.0.0/16"
+  public_subnet_cidr_block = "10.1.1.0/24"
+  private_subnet_cidr_block = "10.1.2.0/24"
+}
+
+ module "ec2-au"{
   providers = {
     aws = aws.AU
   }
   source = "./modules/EC2"
-  ec2_instance_name = "ec2-test"
+  for_each = toset(var.environments)
+  ec2_instance_name = "ec2-${each.value}"
   ec2_instance_type = "t2.micro"
-  vpc_id = module.vpc.vpc_id
-  private_subnet_id = module.vpc.private_subnet_id
-  public_subnet_id = module.vpc.public_subnet_id
+  vpc_id = module.vpc-au[each.value].vpc_id
+  private_subnet_id = module.vpc-au[each.value].private_subnet_id
+  public_subnet_id = module.vpc-au[each.value].public_subnet_id
+  iam_instance_profile = module.permission.aws_iam_instance_profile_id
+}
+
+module "vpc-uk" {
+  providers = {
+    aws = aws.UK
+  }
+  for_each = toset(var.environments)
+  source = "./modules/VPC"
+  vpc_name = "vpc-${each.value}"
+  cidr_block = "10.2.0.0/16"
+  public_subnet_cidr_block = "10.2.1.0/24"
+  private_subnet_cidr_block = "10.2.2.0/24"
+}
+
+ module "ec2-uk"{
+  providers = {
+    aws = aws.UK
+  }
+  source = "./modules/EC2"
+  for_each = toset(var.environments)
+  ec2_instance_name = "ec2-${each.value}"
+  ec2_instance_type = "t2.micro"
+  vpc_id = module.vpc-uk[each.value].vpc_id
+  private_subnet_id = module.vpc-uk[each.value].private_subnet_id
+  public_subnet_id = module.vpc-uk[each.value].public_subnet_id
+  iam_instance_profile = module.permission.aws_iam_instance_profile_id
+}
+
+module "vpc-us" {
+  providers = {
+    aws = aws.US
+  }
+  for_each = toset(var.environments)
+  source = "./modules/VPC"
+  vpc_name = "vpc-${each.value}"
+  cidr_block = "10.3.0.0/16"
+  public_subnet_cidr_block = "10.3.1.0/24"
+  private_subnet_cidr_block = "10.3.2.0/24"
+}
+
+ module "ec2-us"{
+  providers = {
+    aws = aws.US
+  }
+  source = "./modules/EC2"
+  for_each = toset(var.environments)
+  ec2_instance_name = "ec2-${each.value}"
+  ec2_instance_type = "t2.micro"
+  vpc_id = module.vpc-us[each.value].vpc_id
+  private_subnet_id = module.vpc-us[each.value].private_subnet_id
+  public_subnet_id = module.vpc-us[each.value].public_subnet_id
   iam_instance_profile = module.permission.aws_iam_instance_profile_id
 }
